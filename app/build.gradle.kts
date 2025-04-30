4c602c1802b863ee7a36de1d2e3d78fecba62441
luu-9798 <trungluu9798@gmail.com>
2025-04-29 21:26:54 -0400

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,16 +44,34 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
+
+    //Compose BOM(Bill of Material): Sync versions of Compose libraries
+    implementation(platform(libs.androidx.compose.bom))
+
+    //Core Compose UI: basic UI elements like text, column, row, etc.
+    implementation(libs.androidx.ui)
+
+    //Material 3 components: Modern material UI design
     implementation(libs.androidx.material3)
+
+    //Tooling support: @Preview for visualization
+    implementation(libs.androidx.ui.tooling.preview)
+
+    //Navigation for Compose: declarative navigations (NavHost, NavController)
+    implementation(libs.androidx.navigation.compose)
+
+    //Window size class
+    implementation(libs.androidx.material3.window.size.class1)
+
+    //Test implementation
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    //Debug implementation
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
