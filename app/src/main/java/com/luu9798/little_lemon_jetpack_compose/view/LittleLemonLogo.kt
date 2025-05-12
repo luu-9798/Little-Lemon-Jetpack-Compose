41ce57617558af54ec8952ec7ed9fac3ee13e853
luu-9798 <trungluu9798@gmail.com>
2025-05-12 14:10:01 -0400

    The Logo composable

---
+++ app/src/main/java/com/luu9798/little_lemon_jetpack_compose/view/LittleLemonLogo.kt
@@ -0,0 +1,25 @@
+package com.luu9798.little_lemon_jetpack_compose.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.luu9798.little_lemon_jetpack_compose.R
+
+@Composable
+fun LittleLemonIcon() {
+    Image(
+        painter = painterResource(R.mipmap.little_lemon),
+        contentDescription = "Little Lemon Logo",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(16.dp),
+        contentScale = ContentScale.Fit
+    )
+}
